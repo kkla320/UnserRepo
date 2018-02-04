e7b74ae520a52437434d76b52fa106371ebf88e3
Effizjens <jense.92+github@gmail.com>
2018-02-05 00:28:58 +0100

    Ausfallswinkel

--- scripts/physik.js
+++ scripts/physik.js
@@ -1,3 +1,30 @@
 function einstein(m ,c){
     return m*c^2;
 }
+
+function ausfallswinkel(start,kollisonsnormale){
+    var skalar = start.x*kollisonsnormale.x+start.y*kollisonsnormale.y;
+    var length = Math.sqrt(betrag(start))*Math.sqrt(betrag(kollisonsnormale));
+    var phi = Math.acos(skalar/length);
+    var steigung = "";
+    if(phi != 90){
+        steigung = Math.tan(-phi);
+    }else{
+        steigung = (kollisonsnormale.y-start.y)/(kollisonsnormale.x-start.x);
+    }
+    return steigung;
+}
+
+function tangente(x,y){
+    //if classe ball 
+}
+
+function normale(m){
+    return (-1/m);
+}
+
+function betrag(coords){
+    coords.x = x^2;
+    coords.y = y^2;
+    return coords;
+}
