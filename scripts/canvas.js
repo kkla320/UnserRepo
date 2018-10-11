f48682c6ff7f0898d7250a138f34c4a1a96ff9ff
Kevin <accounts_kkla@icloud.com>
2018-10-11 18:32:20 +0200

    Commit

--- scripts/canvas.js
+++
@@ -1,9 +0,0 @@
-var canvasElement = document.getElementById("main");
-var canvas = canvasElement.getContext("2d");
-
-var ball = new Ball(100, 300, 10, canvas);
-ball.render();
-
-
-var ball = new Ball(100, 200, 10, canvas);
-ball.render();
